9ab182e231b12747d04ddc71ab419d63a11c2c45
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2022-03-15 19:44:29 +0300

    assets and some work on the js

--- Sidescroller game/index.js
+++ Sidescroller game/index.js
@@ -1,3 +1,5 @@
+import platform from '/assets/platform.png'
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -41,7 +43,7 @@ class Player{
 }
 
 class Platform{
-    constructor(x, y){
+    constructor({x, y}){
         this.position = {
             x,
             y
@@ -71,42 +73,49 @@ const keys = {
 
 }
 
-player.update();
+let scrollOffset = 0;
 
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
     player.update();
-    platforms.forEach(platform => {
+    platforms.forEach((platform) => {
         platform.draw();
     });
 
     if(keys.right.pressed && player.position.x<400){
+        scrollOffset +=5;
         player.velocity.x = 5;
     }else if(keys.left.pressed && player.position.x>100){
+        scrollOffset -=5;
         player.velocity.x = -5;
     } else {
-        player.velocity.x = 0;
+        player.velocity.x *= 0.9;
 
 
         if(keys.right.pressed){
-            platforms.forEach(platform => {
+            platforms.forEach((platform) => {
                 platform.position.x -= 5;
             });
             
         }
         else if(keys.left.pressed){
-            platforms.forEach(platform => {
+            platforms.forEach((platform) => {
                 platform.position.x += 5;
             });
         }
     }
 // platform detection
-platforms.forEach(platform => {
+platforms.forEach((platform) => {
+
     if(player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
         player.velocity.y = 0;
     }
-})
+});
+
+if (scrollOffset > 2000){
+    console.log('you win');
+}
 
 }
 animate();
